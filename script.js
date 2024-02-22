a66bca0a3f9850c4d4127cba68afb2735068a2db
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-02-22 15:30:40 +0100

    add cganges

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 console.log('ciao')
 
+const containerQ = document.getElementById('my_container');
+
+ for(let i = 1; i <=100; i++ ){
+  if(i % 3 ===0){
+    my_container.innerHTML += `
+    <div class="box dispari">${i}</div>
+    `
+  }else if(i % 5 ===0){
+    my_container.innerHTML += `
+    <div class="box multi5">${i}</div>
+    `
+  }else{
+    my_container.innerHTML += `
+    <div class="box">${i}</div>
+    `
+
+  }
+  
+
+  }
+  my_container.innerHTML
