6723f89a8fa88847095993ff54e62ef298dc190e
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-02-22 15:51:41 +0100

    add changes

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const containerQ = document.getElementById('my_container');
  for(let i = 1; i <=100; i++ ){
   if(i % 3 ===0){
     my_container.innerHTML += `
-    <div class="box dispari">${i}</div>
+    <div class="box dispari">fizz</div>
     `
   }else if(i % 5 ===0){
     my_container.innerHTML += `
-    <div class="box multi5">${i}</div>
+    <div class="box multi5">buzz</div>
     `
   }else{
     my_container.innerHTML += `
