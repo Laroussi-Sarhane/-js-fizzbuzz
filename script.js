8fa37ce4e6e5f26c30b6ef937b48ea38c37a0d0d
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-20 17:59:57 +0100

    add changes

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ console.log('ciao')
 const containerQ = document.getElementById('my_container');
 
  for(let i = 1; i <=100; i++ ){
-  if(i % 3 ===0){
+  if(i % 15 ===0){
     my_container.innerHTML += `
-    <div class="box dispari">fizz</div>
+    <div class="box dispari">fizz-buzz</div>
     `
   }else if(i % 5 ===0){
     my_container.innerHTML += `
     <div class="box multi5">buzz</div>
+    `
+  }else if(i % 3 ===0){
+    my_container.innerHTML += `
+    <div class="box fizz">fizz</div>
     `
   }else{
     my_container.innerHTML += `
